b160f1ef7efd03d7c58e961b932edfef2c7da68b
Anandela Titis Panggraito <anandela.panggraito@tower-bersama.com>
2024-12-17 13:23:04 +0700

    [003] - DEV

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+    return (
+      <footer>
+        <p>&copy; 2024 My Next.js App</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ app/components/Loader.tsx
@@ -0,0 +1,11 @@
+import { ClipLoader, PacmanLoader, HashLoader } from "react-spinners";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <HashLoader size={80} color={"black"} />
+    </div>
+  );
+};
+
+export default MyComponent;
